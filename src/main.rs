e5c8497501423c02135cf51e9f0fb717d67f8128
TeaYouniverse <ph.patiphanofficial@gmail.com>
2026-02-25 01:53:40 +0700

    chore(handling-error): handling error about load config from env on main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 use revenue_service::config::config_loader::load;
+use tracing::{error, info};
 
 #[tokio::main]
 async fn main() {
-    let dotenvy_load = load();
-    println!("{:?}", dotenvy_load)
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    let _ = match load() {
+        Ok(env) => env,
+        Err(e) => {
+            error!("Failed to load env {:?}", e);
+            std::process::exit(1)
+        }
+    };
+
+    info!("ENV has been loaded");
 }
